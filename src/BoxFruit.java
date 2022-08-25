a89174781e5cd8efd932463c3c4b19552848a6d2
Olechka1809 <lemesh.olga@gmail.com>
2022-08-25 23:57:25 +0300

    commit lesson3

--- src/BoxFruit.java
+++ src/BoxFruit.java
@@ -1,4 +1,4 @@
-package ru.geekbrains.qa.java2.lesson3.homeWork;
+package src;
 
 import java.util.ArrayList;
 
--- src/MyArrayDataException.java
+++
@@ -1,7 +0,0 @@
-package Javacore.src.homeWork.javacorehm1.src;
-
-public class MyArrayDataException extends Exception {
-    public MyArrayDataException(String message) {
-        super(message);
-    }
-}
